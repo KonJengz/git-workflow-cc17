151815fd7820bd35415d4743f80c39c3f5003b7b
STEViely <stevielyfiles@gmail.com>
2024-06-19 14:21:31 +0700

    login OK

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-require('dotenv').config()
-const express = require('express')
-const notFound = require('./middlewares/not-found')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const notFound = require("./middlewares/not-found");
+const app = express();
 
-app.use('/register', (req,res) => {
-    res.json({msg: 'register . . .'})
-})
-
-app.use(notFound)
+app.use(notFound);
 app.use(errormiddleware);
 
-const port = process.env.PORT || 8000
-app.listen(port, () => console.log('run port', port))
+app.use("/login", (req, res) => {
+  res.json({ mes: "login..." });
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log("run port", port));
