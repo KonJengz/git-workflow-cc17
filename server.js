46964be38c10a7d4e3c8b47e783273d8e040478a
KonJengz <gecko.jeng@gmail.com>
2024-06-19 14:03:51 +0700

    add register

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express')
 const notFound = require('./middlewares/not-found')
 const app = express()
 
+app.use('/register', (req,res) => {
+    res.json({msg: 'register . . .'})
+})
+
 app.use(notFound)
 app.use(errormiddleware);
 
