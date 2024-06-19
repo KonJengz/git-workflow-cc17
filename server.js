a422f447f952537e3d20fa2b38eb3221a838486d
KonJengz <gecko.jeng@gmail.com>
2024-06-19 12:21:48 +0700

    add page not found

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const express = require('express')
+const notFound = require('./middlewares/not-found')
 const app = express()
+
+app.use(notFound)
 
 const port = process.env.PORT || 8000
 app.listen(port, () => console.log('run port', port))
